7c644fa67db327489f8e3616077691b5e94c2a9d
Hyperion97 <amitc97@gmail.com>
2014-02-05 19:16:48 -0500

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return create_account(request)
+
+def create_account(request):
+    return render(request, 'accounts/create_account.html', {})
---
+++ odin/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+import accounts
+from accounts import urls
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'odin.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^accounts/', include(accounts.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+)
