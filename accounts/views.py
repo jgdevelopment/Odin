b24192b0560629e8b2fd20119d5e8d6d928436e1
Hyperion97 <amitc97@gmail.com>
2014-02-07 20:34:28 -0500

    added create account functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
 
 def index(request):
     return create_account(request)
 
 def create_account(request):
-    return render(request, 'accounts/create_account.html', {})
+    def render_page(user_exists=False):
+        params = {'user_exists': user_exists}
+        return render(request, 'accounts/create_account.html', params)
+        
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        
+        if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+            return render_page(user_exists=True)
+        else:
+            user = User.objects.create_user(username, email, password)
+            return redirect('http://google.com/')
+    else:
+        return render_page()
 
 def home(request):
     return render(request, 'accounts/home.html', {})
